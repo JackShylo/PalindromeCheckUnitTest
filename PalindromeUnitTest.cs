d7f1efd23751a726fdb8f6976ea1f39b5bbfa14c
Jack Kelly <xJaquar7@gmail.com>
2020-03-30 09:02:01 +1300

    Add project files.

---
+++ PalindromeUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using PalindromeChecker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PalindromeCheckUnitTest
+{
+    [TestClass]
+    public class PalindromeUnitTest
+    {
+        Methods myMethod = new Methods();
+
+        [TestMethod]
+        public void ShouldPlaindromeBool()
+        {
+            string Input = "A Man, A Plan, A Canal-Panama!";
+            Boolean Expected = true;
+
+            Boolean Actual = myMethod.CheckIfPalindrome(Input);
+
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod]
+        public void ShouldNotPlaindromeBool()
+        {
+            string Input = "Well I don't think this is a palindrome.";
+            Boolean Expected = false;
+
+            Boolean Actual = myMethod.CheckIfPalindrome(Input);
+
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PalindromeCheckUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PalindromeCheckUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4af01e0-c18e-42b5-b826-356b43130383")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
